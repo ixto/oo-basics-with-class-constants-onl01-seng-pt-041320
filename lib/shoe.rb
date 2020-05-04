0d2d84ffd01522313f00d8badf70c1de978991e0
Joey <joey.alzamora@gmail.com>
2020-05-04 22:12:26 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,7 +5,12 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brands
 
- 
+ def initialize(brand)
+    @brand = brand
+    if !(BRANDS.include?(@brand))
+      BRANDS << brand
+  end
+end
  
 end
   
