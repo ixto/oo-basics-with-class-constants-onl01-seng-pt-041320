bc842e46279101d3490c26ca1b4660d6e1c88a67
Joey <joey.alzamora@gmail.com>
2020-05-04 22:11:19 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,24 +1,30 @@
 class Shoe
+  
+  Brands = []
+  
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_reader :brands
 
-   BRANDS = []
-   
-  # def initialize=(brand)
-  #   @brand = brand
-  #   BRANDS << brand
-  # end
-  
-  def brand=(brand)
+  def initialize=(brand)
     @brand = brand
-    BRANDS << brand
-  end 
-  
+      
  
-
+end
+  
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
  
 end
+
+# array = ["book", "pencil", "notebook"]
+# if !array.include?("book")
+# new_array = []
+# new_array << array
+# end
+
+ # def initialize=(brand)
+  #   @brand = brand
+  #   BRANDS << brand
+  # end
